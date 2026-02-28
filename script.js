294746dff9119bd8f3b6ab44dc57b8dc2e66f227
Megana <megana052003@gmail.com>
2026-02-28 16:46:25 +0530

    Describe what you changed

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+const weatherDescriptions = {
+  0: "Clear Sky",
+  1: "Mainly Clear",
+  2: "Partly Cloudy",
+  3: "Overcast",
+  45: "Fog",
+  48: "Depositing Rime Fog",
+  51: "Light Drizzle",
+  53: "Moderate Drizzle",
+  55: "Dense Drizzle",
+  61: "Slight Rain",
+  63: "Moderate Rain",
+  65: "Heavy Rain",
+  71: "Slight Snow",
+  73: "Moderate Snow",
+  75: "Heavy Snow",
+  95: "Thunderstorm"
+};
+
 document.getElementById("searchBtn").addEventListener("click", () => {
   const city = document.getElementById("cityField").value.trim();
   if (city) {
